6080770b12e22c5705d5dba5e4d6ca0dbc0d9553
Prathicksha <prathicksha.vcse@ksrce.ac.in>
2016-08-30 00:15:01 +0530

    Create Player130.java

---
+++ Player130.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Player130 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String string = scanner.nextLine();
+		int count = 0;
+		int c = 0;
+		for (int i = 0; i < string.length(); i++) {
+			c = string.charAt(i);
+			if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o')
+					|| (c == 'u') || (c == 'A') || (c == 'E') || (c == 'I')
+					|| (c == 'O') || (c == 'U')) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		scanner.close();
+	}
+}
